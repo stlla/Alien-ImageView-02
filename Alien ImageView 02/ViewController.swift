59ba7a34f6468cab2f86eaa1a16a7abb62a8be59
D7702_10 <d7702_10@d7702-10ui-imac.local>
2019-03-28 14:29:16 +0900

    Initial Commit

---
+++ Alien ImageView 02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alien ImageView 02
+//
+//  Created by D7702_10 on 2019. 3. 28..
+//  Copyright © 2019년 D7702_10. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
