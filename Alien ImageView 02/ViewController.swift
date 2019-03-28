d7ef7832473489eb4ebeba0fb78f49e9749a56ea
D7702_10 <d7702_10@d7702-10ui-imac.local>
2019-03-28 15:30:13 +0900

    3/28일 버튼을 이용해 이미지를 움직이는 코딩

--- Alien ImageView 02/ViewController.swift
+++ Alien ImageView 02/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var alienImagerViewer: UIImageView!
+    @IBOutlet weak var IBOutlet: UILabel!
+    
+    
+    var count = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+            alienImagerViewer.image = UIImage(named: "frame1.png")
+
+        IBOutlet.text = "frame\(count).png"
     }
 
 
+    @IBAction func IBAction(_ sender: Any) {
+        count+=1
+        
+        if(count>5){
+            count = 1
+        }
+        alienImagerViewer.image = UIImage(named: "frame\(count).png")
+        
+        IBOutlet.text = "frame\(count).png"
+    }
 }
 
